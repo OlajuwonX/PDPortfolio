833d6d8d3885a8cd1c6002257a53983b8e54de2d
OlajuwonX <olasimboolajuwon@gmail.com>
2025-09-24 13:37:14 +0100

    lazy loading and logo optimization

--- src/components/mobile.tsx
+++ src/components/mobile.tsx
@@ -39,7 +39,7 @@ const Mobile = () => {
                     navigate("/");
                     setOpen(false);
                 }}>
-                    <img src="/pdlogo1.png" loading="lazy" alt="NavLogo" className="h-10 w-11 cursor-pointer" />
+                    <img src="/pdlogo2.webp" loading="lazy" alt="NavLogo" className="h-10 w-11 cursor-pointer" />
                 </button>
             </div>
 
